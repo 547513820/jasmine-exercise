bd5cb29503c66e8037a33728c9571fef0da8d0d0
547513820 <tomlovewhat@gmail.com>
2021-07-07 02:47:02 -0700

    part one

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -6,12 +6,26 @@ describe("Servers test (with setup and tear-down)", function() {
 
   it('should add a new server to allServers on submitServerInfo()', function () {
     submitServerInfo();
-
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('test for function submitServerInfo when the input is empty',function(){
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(serverId).toEqual(0);
+  });
+  it('test for function updateServerTable if it add a new server',function(){
+    allServers['server1'] = "Alice";
+    allServers['server2'] = "Alice2";
+    allServers['server3'] = "Alice3";
+    updateServerTable();
+    expect(document.querySelectorAll("#serverTable tbody tr").length).toEqual(3);
+  });
+
   afterEach(function() {
-    // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
